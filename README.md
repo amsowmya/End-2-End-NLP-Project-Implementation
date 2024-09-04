04f4b8e401148ad740c6cfd402b15297bbdd172a
Sowmya AM <2260927@cognizant.com>
2024-09-04 15:26:08 +0530

    added logging and exception

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # End-2-End-NLP-Project-Implementation
+
+## How to run
+
+```bash
+conda create -n hate  python==3.10 -y
+```
+
+```bash
+conda activate hate
+```
+
+```bash
+pip install -r requirements.txt
+```
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="HateSpeechProject",
+    description="Find the hate speech",
+    author="Sowmya AM",
+    author_email="sowmya.anekonda@gmail.com",
+    packages=find_packages()
+)
